cf62df76aa28ffdd75fa7846ced852a992bd6fa4
Lucas Stapf <lucas.c.stapf@gmail.com>
2023-05-03 20:13:41 -0300

    Várias mudanças na plotagem do grafo.

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
 from models.grafo import Grafo
 from random import randrange, uniform
+from dash import dcc, html
+from dash.dependencies import Input, Output
+import models.grafo as gf
 import models.plot as mp
 import plotly.graph_objects as go
 import dash
-from dash import dcc, html
-from dash.dependencies import Input, Output
 
 app = dash.Dash()
 g = Grafo(n_range=100)
-g.gerar_vertices()
-g.gerar_arestas(0.05)
+passos = [[]]
+origem = destino = 0
 
 @app.callback(Output('live-update-graph', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_figure(n):
-    for vertex in g.vertices:
-        p = uniform(0, 1)
-        if (p > 0.5):
-            vertex.visitado = True
-        else:
-            vertex.visitado = False
-    
-    return mp.update_grafo(g)
+    index = n
+    if n >= len(passos):
+        index = len(passos) - 1
+    # print(f"Index :{index} List: {passos[index]}")
+    return mp.update_grafo(g, g.vertice_indice(origem), g.vertice_indice(destino), passos[index])
+
+@app.callback(
+    [Output(component_id='interval-component', component_property='interval')],
+    [Input('interval-refresh', 'value')])
+def update_refresh_rate(value):
+    return [value * 1000]
     
 if __name__ == "__main__":
 
+    g.gerar_vertices()
+    g.gerar_arestas(0.05)
+    
+    origem = randrange(g.range)
+    destino = randrange(g.range)
+    while origem == destino:
+        destino = randrange(g.range)
+
+    gf.busca_profundidade(g, origem, destino, passos, True)
+
     app.layout = html.Div([
+        html.H1('Rede Aleatória'),
         dcc.Graph(id='live-update-graph'),
         dcc.Interval(
             id='interval-component',
             interval=1*1000, # in milliseconds
             n_intervals=0
-        )
+        ),
+        html.Div(title='Delay (s)', children=[
+        dcc.Slider(min=0.5, max=2, step=0.5, value=1, id='interval-refresh'
+        ), 
+    ])
     ])
 
-    app.run_server(debug=True)
+    app.run(debug=True)
--- models/plot.py
+++ models/plot.py
@@ -1,20 +1,23 @@
+from models.grafo import Grafo
+from models.vertice import Vertice
 import plotly.graph_objects as go
 
-def add_edge(fig, x, y, color):
+def add_edge(fig: go.Figure, x: int, y: int, name: str, color):
     fig.add_trace(
         go.Scatter(
+        name=name,
         x=x, y=y,
         line=dict(width=0.5, color=color),
-        hoverinfo='none',
         mode='lines')
     )
 
-def add_vertex(fig, x, y, color):
+def add_vertex(fig: go.Figure, x: int, y: int, name: str, color):
     fig.add_trace(
         go.Scatter(
+        name=name,
         x=x, y=y,
         mode='markers',
-        hoverinfo='text',
+        hovertemplate='(%{x}, %{y})',
         marker=dict(
             # showscale=True,
             # colorscale options
@@ -25,51 +28,66 @@ def add_vertex(fig, x, y, color):
             # reversescale=True,
             color=color,
             size=10,
-            colorbar=dict(
-                thickness=15,
-                title='Node Connections',
-                xanchor='left',
-                titleside='right'
-            ),
+            # colorbar=dict(
+            #     thickness=15,
+            #     title='Node Connections',
+            #     xanchor='left',
+            #     titleside='right'
+            # ),
             line_width=1)
         )
     )
 
-def update_grafo(g):
+def update_grafo(g: Grafo, origem: Vertice, destino: Vertice, visitados) -> go.Figure:
 
     fig = go.Figure()
 
     edge_x = []
     edge_y = []
+    edge_x_visitados = []
+    edge_y_visitados = []
+
     for i in range(g.range):
         v1 = g.vertice_indice(i)
         for d in v1.vizinhos:
             if d[0] < i: pass
-            edge_x.append(v1.x)
-            edge_y.append(v1.y)
-            v2 = g.vertice_indice(d[0]) 
-            edge_x.append(v2.x)
-            edge_y.append(v2.y)
-            edge_x.append(None)
-            edge_y.append(None)
+            v2 = g.vertice_indice(d[0])
+            if i in visitados and d[0] in visitados:
+                edge_x_visitados.append(v1.x)
+                edge_y_visitados.append(v1.y)
+                edge_x_visitados.append(v2.x)
+                edge_y_visitados.append(v2.y)
+                edge_x_visitados.append(None)
+                edge_y_visitados.append(None)
+            else:
+                edge_x.append(v1.x)
+                edge_y.append(v1.y)
+                edge_x.append(v2.x)
+                edge_y.append(v2.y)
+                edge_x.append(None)
+                edge_y.append(None)
 
 
-    add_edge(fig, edge_x, edge_y, '#999')
+    add_edge(fig, edge_x, edge_y, 'Não percorrida', '#999')
+    add_edge(fig, edge_x_visitados, edge_y_visitados, 'Percorrida', '#000')
 
     node_x_visitado = []
     node_y_visitado = []
     node_x = []
     node_y = []
 
-    for node in g.vertices:
-        if not node.visitado:
-            node_x.append(node.x)
-            node_y.append(node.y)
-        else:
+    for n in range(g.range):
+        node = g.vertice_indice(n)
+        if  n in visitados:
             node_x_visitado.append(node.x)
             node_y_visitado.append(node.y)
+        else:
+            node_x.append(node.x)
+            node_y.append(node.y)
 
-    add_vertex(fig, node_x, node_y, '#000')
-    add_vertex(fig, node_x_visitado, node_y_visitado, '#298')
+    add_vertex(fig, node_x, node_y, 'Não visitados', '#000')
+    add_vertex(fig, node_x_visitado, node_y_visitado, 'Visitados', 'blue')
+    add_vertex(fig, [origem.x], [origem.y], 'Origem', 'green')
+    add_vertex(fig, [destino.x], [destino.y], 'Destino', 'red')
     
     return fig
