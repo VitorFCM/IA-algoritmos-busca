12ee458616f11d8f16c0fe361820603361ed980c
VitorFCM <vitor.carreramiguel@gmail.com>
2023-05-22 22:53:12 -0300

    Coletando os dados sobre os custos encontrados

--- avaliacao.py
+++ avaliacao.py
@@ -1,11 +1,12 @@
 from models.grafo import Grafo
 from auxiliar.tempo import mede_tempo
 from auxiliar.grafico import gera_grafico
-from random import randrange
-import sys
-
+from searches.buscaInformada import busca_informada
+from searches.funcoesAvaliacao import *
 from searches.profundidade import busca_profundidade
 from searches.largura import busca_largura
+from random import randrange
+import sys
 
 if __name__ == "__main__":
 
@@ -15,32 +16,45 @@ if __name__ == "__main__":
 
     valores_p = [0.05, 0.025, 0.01]
     valores_tempo = [0.0, 0.0, 0.0, 0.0]
+    valores_custo = [0.0, 0.0, 0.0, 0.0]
     n_testes = 10
 
     for p in valores_p:
         
-        grafo = Grafo(200, p)
+        grafo = Grafo(2000, p)
         
         for i in range(n_testes):
 
             origem = randrange(grafo.range)
             destino = randrange(grafo.range)
-            while origem == destino:
+            while origem == destino or busca_informada(grafo, origem, destino, heuristica_best_first, info_rede_best_first) == -1:
                 destino = randrange(grafo.range)
 
-            print("Origem: " + str(origem) + " destino: " + str(destino))
+            #Busca em profundidade
+            tempo, custo = mede_tempo(busca_profundidade(grafo, origem, destino))
+            valores_tempo[0] += tempo
+            valores_custo[0] += custo
 
-            valores_tempo[0] += mede_tempo(busca_profundidade(grafo, origem, destino))
-            valores_tempo[1] += mede_tempo(busca_largura(grafo, origem, destino))
+            #Busca em largura
+            #todo
+
+            #Best first
+            tempo, custo = mede_tempo(busca_informada(grafo, origem, destino, heuristica_best_first, info_rede_best_first))
+            valores_tempo[2] += tempo
+            valores_custo[2] += custo
+
+            #A*
+            tempo, custo = mede_tempo(busca_informada(grafo, origem, destino, heuristica_otimista, info_rede_A_estrela))
+            valores_tempo[3] += tempo
+            valores_custo[3] += custo
 
         
         medias_tempo = map(lambda total : total/n_testes, valores_tempo)
         medias_tempo = list(medias_tempo)
 
-        gera_grafico(valores_y=medias_tempo, titulo="Tempos - n=2000 p=" + str(p*100))
+        medias_custo = map(lambda total : total/n_testes, valores_custo)
+        medias_custo = list(medias_custo)
 
-    #found = busca_informada(grafo, start_node, goal_node, heuristica_best_first, info_rede_best_first)
-    #if found:
-    #    print("Caminho encontrado!")
-    #else:
-    #    print("Caminho não encontrado.")
+        gera_grafico(valores_y=medias_tempo, titulo="Tempos - n=2000 p=" + str(p*100) + "%")
+        gera_grafico(valores_y=medias_custo, titulo="Custos - n=2000 p=" + str(p*100) + "%")
+
